16d082185aa519dc666b4e2972a3d0544e4362d0
pyaepyaelily <pyaepyaelily@gmail.com>
2023-11-09 14:05:52 +0800

    added header and footer

--- asset/js/header.js
+++ asset/js/header.js
@@ -6,6 +6,9 @@ class Header extends HTMLElement {
       this.innerHTML = `
       <head>
         <link rel="stylesheet" href="asset/style/header.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Source+Sans+3:wght@300&display=swap" rel="stylesheet">
       </head>
     <body>
         <div class="header">
